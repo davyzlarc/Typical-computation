c18e9cbb71250a7913052188585fad222297c958
Влад Карелин <vvlaad@mail.ru>
2020-01-12 22:10:37 +0300

    minor bugs fixed

--- Lab8/Properties/AssemblyInfo.cs
+++ Lab8/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Lab7")]
+[assembly: AssemblyTitle("Lab8")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Lab7")]
+[assembly: AssemblyProduct("Lab8")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
