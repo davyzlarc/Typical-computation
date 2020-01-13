1c14a6f2f1a778f2122fdc1da4a316a64de9a93e
Влад Карелин <vvlaad@mail.ru>
2020-01-13 05:46:24 +0300

    bug fixed

--- Lab8/MainWindow.Designer.cs
+++ Lab8/MainWindow.Designer.cs
@@ -217,7 +217,6 @@
             // 
             // MainWindow
             // 
-            this.FormClosing += MainWindow_FormClosing;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(977, 483);
@@ -240,6 +239,7 @@
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.KeyPreview = true;
             this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximizeBox = false;
             this.Name = "MainWindow";
             this.Text = "Типовой расчет - Карелин Влад , группа А-08-18";
             this.ResumeLayout(false);
